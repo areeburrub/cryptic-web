776d7ec3212b80e3ef291146df772a6aed1ffe33
areeburrub <areeburrub@gmail.com>
2022-05-06 00:52:11 +0530

    team admin styling fix

--- pages/admin/team.js
+++ pages/admin/team.js
@@ -35,7 +35,7 @@ function Team() {
   }
 
   //  State
-  const [ModalOpen, setModalOpen] = useState(false)
+  const [ModalOpen, setModalOpen] = useState(true)
   const [TeamModalData, setTeamModalData] = useState({
     tid:makeid(6),
     teamName: ""
