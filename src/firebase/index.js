f26a5c508060f8955ecd3edd2fa9d782b0d98952
areeburrub <areeburrub@gmail.com>
2022-05-08 19:53:40 +0530

    FIrebase update

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 const config = {
-  apiKey: "AIzaSyAv0fautzu7-WGY0ZOYQwxTQAB3ZpDH2PI",
-  authDomain: "cryptic-web-2022.firebaseapp.com",
-  projectId: "cryptic-web-2022",
-  storageBucket: "cryptic-web-2022.appspot.com",
-  messagingSenderId: "1086019378140",
-  appId: "1:1086019378140:web:959d666af3bfde4a744d19",
-  measurementId: "G-86XW9BCZ7D",
+  apiKey: "AIzaSyBTFTIUg8Z-C3TZu5_KwLUdx-y1BZ1rK6o",
+  authDomain: "cryptic-hunt-web.firebaseapp.com",
+  projectId: "cryptic-hunt-web",
+  storageBucket: "cryptic-hunt-web.appspot.com",
+  messagingSenderId: "236402709063",
+  appId: "1:236402709063:web:b8d980ec6ddee13c769762",
+  measurementId: "G-0CVVGVXSYV",
 };
 
 
